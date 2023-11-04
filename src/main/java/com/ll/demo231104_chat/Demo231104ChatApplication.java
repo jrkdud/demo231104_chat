571e1b68d992a45850424dad1e31214bfeec88cd
jrkdud <newgy28@naver.com>
2023-11-04 13:36:48 +0900

    1번 채팅방

--- src/main/java/com/ll/demo231104_chat/Demo231104ChatApplication.java
+++ src/main/java/com/ll/demo231104_chat/Demo231104ChatApplication.java
@@ -2,9 +2,11 @@ package com.ll.demo231104_chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.Mapping;
 
 @SpringBootApplication
 public class Demo231104ChatApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo231104ChatApplication.class, args);
