08a1e816496665cfa551b611861eb8c89e176bb8
jrkdud <newgy28@naver.com>
2023-11-04 13:16:54 +0900

    프로젝트 생성

---
+++ src/main/java/com/ll/demo231104_chat/Demo231104ChatApplication.java
@@ -0,0 +1,13 @@
+package com.ll.demo231104_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo231104ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo231104ChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/demo231104_chat/Demo231104ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.demo231104_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo231104ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
